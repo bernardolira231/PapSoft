82cf5c1a322f86149a69ac670a673d789549fd99
Bernardo Lira <harritolira@gmail.com>
2023-04-22 14:07:04 -0600

    use db

--- NetworkDiscover/views.py
+++ NetworkDiscover/views.py
@@ -29,6 +29,38 @@ def start(request):
                 'password': ssh_password
             }
             net_connect = ConnectHandler(**device)
+
+            output = net_connect.send_command('show inventory')
+
+            for line in output.splitlines():
+                if 'Chassis' in line:
+                    if 'router' in line.lower():
+                        sor = 'router'
+                    elif 'switch' in line.lower():
+                        sor = 'switch'
+                    else:
+                        sor = 'desconocido'
+                    break
+            else:
+                sor = 'desconocido'
+
+            output = net_connect.send_command('show run | i hostname')
+            hostname = output.split()[1]
+
+            device = Device.objects.create(
+                device_username=request.POST['device_username'],
+                device_password=request.POST['device_password'],
+                device_ip=request.POST['device_ip'],
+                device_type=sor,
+                device_name=hostname,
+            )
+            device.save()
+
+            output = net_connect.send_command('show cdp neighbors detail')
+            for line in output.splitlines():
+                if 'IP address: ' in line:
+                    new_ip_address = line.split('IP address: ')[1]
+
             return redirect('/show_device', net_connect)
         except:
             return render(request, 'start.html', {
