c2c93759f8b3f56938459b55883fc61c83e8f96c
Bernardo Lira <harritolira@gmail.com>
2023-05-02 12:33:10 -0600

    Add neighbors in the DB

--- NetworkDiscover/models.py
+++ NetworkDiscover/models.py
@@ -10,6 +10,7 @@ class Device(models.Model):
     device_ip = models.CharField(max_length=100)
     device_username = models.CharField(max_length=100)
     device_password = models.CharField(max_length=100)
+    neighbors = models.ManyToManyField('self', blank=True)
 
     def __str__(self):
         return self.device_name + ' - ' + self.device_ip
