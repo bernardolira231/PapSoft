0b435231df49a151ef79bc28fa306ccb5a032eb1
Bernardo Lira <harritolira@gmail.com>
2023-05-04 07:24:28 -0600

    Change dbsqlite3 to mysql and add gitignore

--- NetworkDiscover/models.py
+++ NetworkDiscover/models.py
@@ -10,7 +10,6 @@ class Device(models.Model):
     device_ip = models.CharField(max_length=16)
     device_username = models.CharField(max_length=100)
     device_password = models.CharField(max_length=100)
-    ip_neighbor = models.ArrayField(models.CharField(max_length=15))
 
     def __str__(self):
         return self.device_name + ' - ' + self.device_ip
