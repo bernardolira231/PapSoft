3dfdf27b618908b8c3a40341568f6239e63bd492
Bernardo Lira <harritolira@gmail.com>
2023-05-04 07:34:39 -0600

    Change to use django-environ

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'network_discover',
         'USER': 'root',
-        'PASSWORD': 'Jeronimo108',
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
